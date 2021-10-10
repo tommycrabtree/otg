9a3d313506239fbf9df1a47fa5edbb5e62f37d1a
Tommy <tommycrabtree@outlook.com>
2021-10-10 01:58:03 -0500

    9 Building the Shop UI

---
+++ client/src/app/shop/shop.service.ts
@@ -0,0 +1,53 @@
+import { HttpClient, HttpParams, HttpResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { IPagination } from '../shared/models/pagination';
+import { IBrand } from '../shared/models/brand';
+import { IProductType } from '../shared/models/productType';
+import { ShopParams } from '../shared/models/shopParams';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ShopService {
+  baseUrl = 'https://localhost:5001/api/';
+
+  constructor(private http: HttpClient) {}
+
+  getProducts(shopParams: ShopParams): Observable<IPagination> {
+    let params = new HttpParams();
+
+    if (shopParams.brandId !== 0) {
+      params = params.append('brandId', shopParams.brandId.toString());
+    }
+
+    if (shopParams.typeId !== 0) {
+      params = params.append('typeId', shopParams.typeId.toString());
+    }
+
+    if (shopParams.search) {
+      params = params.append('search', shopParams.search);
+    }
+
+    params = params.append('sort', shopParams.sort);
+    params = params.append('pageIndex', shopParams.pageNumber.toString());
+    params = params.append('pageIndex', shopParams.pageSize.toString());
+
+    return this.http.get<IPagination>(this.baseUrl + 'products',
+      {observe: 'response', params})
+      .pipe(
+        map(response => {
+          return response.body;
+        })
+      );
+  }
+
+  getBrands(): Observable<IBrand[]> {
+    return this.http.get<IBrand[]>(this.baseUrl + 'products/brands');
+  }
+
+  getTypes(): Observable<IProductType[]> {
+    return this.http.get<IProductType[]>(this.baseUrl + 'products/types');
+  }
+}
