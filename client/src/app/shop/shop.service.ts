7f79a0d90f5d6f9d7ea6e64576a7cd41dc6f1a1b
Tommy <tommycrabtree@outlook.com>
2021-10-12 18:09:40 -0500

    10 Client Routing

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -6,6 +6,7 @@ import { IPagination } from '../shared/models/pagination';
 import { IBrand } from '../shared/models/brand';
 import { IProductType } from '../shared/models/productType';
 import { ShopParams } from '../shared/models/shopParams';
+import { IProduct } from '../shared/models/product';
 
 @Injectable({
   providedIn: 'root'
@@ -41,6 +42,10 @@ export class ShopService {
           return response.body;
         })
       );
+  }
+
+  getProduct(id: number): Observable<IProduct> {
+    return this.http.get<IProduct>(this.baseUrl + 'products/' + id);
   }
 
   getBrands(): Observable<IBrand[]> {
